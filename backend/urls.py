ea120420f5fc86ac0ee9a350c7d5e12a02e45c8c
Rye-Guy <ryanracioppo95@gmail.com>
2019-08-30 14:48:45 -0400

    added route for react app

--- backend/urls.py
+++ backend/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_jwt.views import obtain_jwt_token
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('token-auth/', obtain_jwt_token),
-    path('nfl/', include('nfl.urls'))
+    path('nfl/', include('nfl.urls')),
+    path('', TemplateView.as_view(template_name='index.html'))
 ]
 
