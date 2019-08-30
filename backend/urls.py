178417e4db315d775affca825609040cd53ce8ae
Rye-Guy <ryanracioppo95@gmail.com>
2019-08-30 15:20:21 -0400

    react template

--- backend/urls.py
+++ backend/urls.py
@@ -17,11 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_jwt.views import obtain_jwt_token
 from django.views.generic import TemplateView
+from nfl.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('token-auth/', obtain_jwt_token),
     path('nfl/', include('nfl.urls')),
-    path('', TemplateView.as_view(template_name='index.html'))
+    path('', index)
 ]
 
